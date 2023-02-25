9d58281e6e3b2a60b624aad6d949b95f38642969
OussamaHaddi <oussama.turboxxx1@gmail.com>
2023-02-25 18:04:03 +0100

    add home page with style and covers and parse it to componenets

---
+++ resources/views/components/cover.blade.php
@@ -0,0 +1,7 @@
+
+
+@for ($i = 1; $i <= 6; $i++)
+<div class="bg-gray-200 rounded-xl shadow-[0_3px_4px] shadow-yellow-500 overflow-hidden cursor-pointer transition-all duration-500 hover:-translate-y-2 hover:opacity-70 ">
+    <img class="" src="{{asset('images/cover '.$i.'.jpg')}}" alt=""/>
+</div>
+@endfor
---
+++ resources/views/partials/_search.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center w-4/6 relative">
+    <form action="" class="w-5/6 relative flex">
+        <input type="search" name="search" class="rounded-full pl-4 py-2 w-full" placeholder="search music">
+        <button class="bg-yellow-400 text-black font-semibold rounded-full px-4 absolute right-1 top-1/2 -translate-y-1/2 py-1 hover:bg-yellow-600">search</button>
+    </form>
+</div>
